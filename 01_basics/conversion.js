0853666c29e56a6398337c880073a88ff372edd5
Ummey Kulsum <ummeykulsumcse@gmail.com>
2025-06-24 17:34:09 +0000

    conversions js code

---
+++ 01_basics/conversion.js
@@ -0,0 +1,33 @@
+// let score = 43;
+let score ="43anc";
+
+console.log(typeof(score));
+console.log(score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+
+let name = "KULSUM";
+let userName = Number(name);
+console.log(userName);
+console.log( typeof userName);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+// let isLoggedIn = 1;
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+
+
+
+
+

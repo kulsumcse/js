15b5861460e179a7ea68e13484d2881c6d59c81f
Ummey Kulsum <ummeykulsumcse@gmail.com>
2025-06-24 14:22:51 +0000

    DataTypes code for bigenner.

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,39 @@
+/*
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+
+//string
+//const userName = "Ummey Kulsum" + 24;   //You can use but not readable code.
+const userName = "Ummey Kulsum";
+console.log(userName);
+console.log (typeof userName);
+
+//Number
+let accountId = 2902398;
+console.log(accountId);
+console.log (typeof accountId);
+
+//BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log(bigNumber);
+console.log (typeof bigNumber);
+
+//Boolean
+let isStudent = true;
+console.log(isStudent);
+console.log (typeof isStudent);
+
+//Undefined
+let accountCity;
+console.log (typeof accountCity);
+
+//Null means Object
+let userEmail = null;
+console.log (typeof userEmail);

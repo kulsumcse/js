26f7b98dad569fe7637d7229b90b605f70641ba3
Ummey Kulsum <ummeykulsumcse@gmail.com>
2025-06-25 04:23:02 +0000

    Data type summery

---
+++ 01_basics/datatypesSummery.js
@@ -0,0 +1,41 @@
+// Primative
+
+// 7-Types  String, Number,Boolearn, Null, Undefined,Symbol,BigInt.
+
+const userName = "UMMEY";
+const score = 100;
+const scoreValue = 200.0;
+const valueInNumber = true;
+let city;
+const outsideTem = undefined;
+/*
+id and anotherid not equel.
+*/
+const id = Symbol('123');
+const anotherid = Symbol('123');
+
+// console.log(id == anotherid);
+
+const bigNumber = 372597910910038998n;
+
+
+
+//  Referance(Non-Primative)  
+
+//  Array, Object,Function.
+
+const uctcDept = ['CSE','EEE','ME'];
+
+let myObj = {
+   Name: 'Kulsum',
+   Age : 24,
+}
+
+const myFunction = function(){
+console.log('Hello uctc')
+}
+console.log(typeof (uctcDept));
+
+
+
+
